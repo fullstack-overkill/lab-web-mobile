8a11db78cd664086f9c5a637bf103a3b5e461d0c
fullstack-overkill <christophermms.ctt@gmail.com>
2022-08-08 03:38:39 -0300

    refactor: change the structure of the initial project

--- 1/src/main.js
+++ 1/src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue';
-import App from './App.vue';
-import './registerServiceWorker';
+import './serviceWorker/registerServiceWorker';
 import router from './router';
 import store from './store';
+import App from './views/app.vue';
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+app.mount('#app');
